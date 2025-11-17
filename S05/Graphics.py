5e5c78081543a9d110d4bb50866abe72aa76ff3e
IanyAlmasanu <iany4all@gmail.com>
2025-11-17 10:52:10 +0200

    uploaded s07

--- S05/Graphics.py
+++ S05/Graphics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import seaborn as sb
+import pandas as pd
 import matplotlib
 from matplotlib import pyplot as plt
 from openpyxl.styles.alignment import vertical_aligments
@@ -13,7 +14,7 @@ def correlogram(R2, dec=2, title='Correlogram', valMin=-1, valMax=1):
     sb.heatmap(data=R2, vmin=valMin, vmax=valMax, cmap='bwr', annot=True)
 
 #correlation circle
-def correlation_circle(R2, v1=0, v2=1, title='Correlation circle'):
+def correlation_circle(R2, v1=0, v2=1, dec=2, title='Correlation circle'):
     plt.figure(num=title, figsize=(8,7))
     plt.title(label=title, fontsize=12, color='Blue', verticalalignment='bottom')
     #draw a circle of radius 1
@@ -25,8 +26,35 @@ def correlation_circle(R2, v1=0, v2=1, title='Correlation circle'):
     plt.axvline(x=0, color='Green')
 
     if isinstance(R2, np.ndarray):
+        plt.xlabel(xlabel='Variable'+ str(v1 + 1), fontsize=10, color='Blue', verticalalignment='bottom')
+        plt.ylabel(ylabel='Variable'+ str(v1 + 1), fontsize=10, color='Blue', verticalalignment='bottom')
         plt.scatter(x=R2[:, v1], y=R2[:, v2], color='Red')
+        for i in range(R2.shape[0]):
+            #same condition as before, except we take i
+            plt.text(x=R2[i, v1], y=R2[i, v2], color='Black',
+                     s='('+(str(np.round(R2[i,v1], decimals=dec))+', ' +
+                        str(np.round(R2[i,v2], decimals=dec))+')'))
+    elif isinstance(R2, pd.DataFrame):
+        #axis labels
+        plt.xlabel(xlabel=R2.columns[v1], fontsize=10, color='Blue', verticalalignment='bottom')
+        plt.ylabel(ylabel=R2.columns[v2], fontsize=10, color='Blue', verticalalignment='bottom')
+        #plt.scatter(x=R2.values[:,v1], y=R2.values[:,v2], color = 'Blue')
+        plt.scatter(x=R2.iloc[:].iloc[v1], y=R2.iloc[:].iloc[v2], color = 'Blue')
+        for i in range(R2.index.size):
+            plt.text(x=R2.iloc[i, v1], y=R2.iloc[i, v2], color='Black',
+                     s=R2.index[i])
+    else:
+        raise Exception('Invalid data type')
 
+#simple line graphic
+def explained_variance(eigenvalues, title='PCA - Explained variance by principal components'):
+    plt.figure(num=title, figsize=(10,8))
+    plt.title(label=title, fontsize=12, color='Blue', verticalalignment='bottom')
+    plt.xlabel(xlabel='Principal components', fontsize=10, color='Blue', verticalalignment='bottom')
+    plt.ylabel(ylabel='Explained variance', fontsize=10, color='Blue', verticalalignment='bottom')
+    components = ['C'+str(i+1) for i in range(len(eigenvalues))]
+    plt.plot(components, eigenvalues, 'o-')
+    plt.axhline(y=1, color='Red')
 
 def show():
     plt.show()
--- S05/Seminar_05.py
+++ S05/Seminar_05.py
@@ -4,9 +4,9 @@ import pandas as pd
 matrix = np.random.uniform(low=1,high=10, size=(30,15))
 
 corr = np.corrcoef(x=matrix, rowvar=False)
-print(corr, type(corr), corr.shape)
+#print(corr, type(corr), corr.shape)
 
-g.correlogram(R2=corr)
+#g.correlogram(R2=corr)
 
 corr_df= pd.DataFrame(data=corr, index=('V'+str(i+1) for i in range(corr.shape[0])),
                       columns=('V'+str(j+1) for j in range (corr.shape[1])))
@@ -15,6 +15,22 @@ corr_df= pd.DataFrame(data=corr, index=('V'+str(i+1) for i in range(corr.shape[0
 
 
 #call in the correlation circle
-#g.correlation_circle(R2=corr)
-g.correlation_circle('what')
+g.correlation_circle(R2=corr)
+
+#g.correlation_circle('what')
+#call the correlation circle passing a pandas.DataFrame
+g.correlation_circle(R2=corr_df, title ='Correlation circle from a pandas.DataFrame')
+#g.show()
+
+#create a vector of 12 random values in [0.3 , 3)
+#values = np.random.uniform(low=0.3, high=3, size=(12,3))
+values = np.random.uniform(low=0.3, high=3, size=12)
+print (np.round(values, 2), type(values))
+values = np.sort(a=values)
+#values in descending order
+print(np.round(values[::-1], 2))
+
+#call the function for the explained variance by the principal components (PCA)
+#values in descending order
+g.explained_variance(eigenvalues = values[::-1])
 g.show()
